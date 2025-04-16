6fb09b481e1da98accf924c8eed67f4da4fc8f10
John Dough <bridgerparrot@gmail.com>
2025-04-15 23:23:03 -0600

    First

--- TicTacToe.Logic/Board.cpp
+++ TicTacToe.Logic/Board.cpp
@@ -1,24 +1,42 @@
 #include "pch.h"
 #include "Board.h"
 
-
 void Board::SetBoard(char setup[3][3])
 {
-	
+
 }
 
 char Board::GetSquare(int row, int col) const
 {
-	return ' ';
+    // Checks if row and column are valid
+    if (row >= 0 && row < 3 && col >= 0 && col < 3) 
+    {
+        return grid[row][col];
+    }
+    return ' ';
 }
 
 bool Board::SetSquare(int row, int col, char player)
 {
-	return false;
+    // within bounds
+    if (row < 0 || row >= 3 || col < 0 || col >= 3) 
+    {
+        return false;
+    }
+
+    // checks if empty
+    if (grid[row][col] != ' ') 
+    {
+        return false;
+    }
+
+    // symbol set
+    grid[row][col] = player;
+    return true;
 }
 
 char Board::GameStatus()
 {
-	return ' ';
+    
+    return ' ';
 }
-
